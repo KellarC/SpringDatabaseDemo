845696a5d957985f96de5183b56c355987aaadb3
KellarC <kellarcarson@gmail.com>
2023-12-07 21:30:23 -0600

    Initial commit

---
+++ src/main/java/kellarc/springdatabasedemo/springdatabasedemo/SpringDatabaseDemoApplication.java
@@ -0,0 +1,13 @@
+package kellarc.springdatabasedemo.springdatabasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDatabaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDatabaseDemoApplication.class, args);
+	}
+
+}
